abb6009c86838cd7cdb5c3ea272e323a435669ad
Clint Edwards <cedwards1080@gmail.com>
2021-10-21 13:21:55 -0600

    removing env from github

--- server/controllers/kennelController.js
+++ server/controllers/kennelController.js
@@ -3,10 +3,7 @@ module.exports = {
   postAnimal: async (req, res) => {
     try {
       const { kennelName } = req.body;
-      const newKennel = await pool.query(
-        "INSERT INTO kennel (name) VALUES($1)",
-        [kennelName]
-      );
+      await pool.query("INSERT INTO kennel (name) VALUES($1)", [kennelName]);
     } catch (err) {
       console.log(err.message);
     }
