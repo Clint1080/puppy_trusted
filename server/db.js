241ec5c67c9851d6b086e2d31d4c928ada47bfbb
Clint Edwards <cedwards1080@gmail.com>
2021-11-03 12:55:36 -0600

    more styles and removed secret from db connection string

--- server/db.js
+++ server/db.js
@@ -10,8 +10,7 @@ require("dotenv").config();
 // };
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig =
-  "postgres://cgttyidabjisiy:a78537290c11091b9e6aa905d80f5d4a545da3b96bc0586bccb15e3ad7d16172@ec2-18-232-216-229.compute-1.amazonaws.com:5432/d44j243fgvh5a0";
+const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString:
